df1aded5d2a5b790c850a5ee6b43b92fe068e873
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2024-04-22 23:52:41 -0300

    feat: adicionando validação ao método Create do controller tarefas

--- src/server/controllers/tarefas/Create.ts
+++ src/server/controllers/tarefas/Create.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from "express";
-
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ITarefa {
     titulo: string;
+    data: Date;
 }
 
-export const create = (req: Request<{}, {}, ITarefa>, res: Response) => {
+const bodyValidation: yup.ISchema<ITarefa> = yup.object().shape({
+    titulo: yup.string().required(),
+    data: yup.date().required()
+});
 
-    console.log(req.body);
+export const create = async (req: Request<{}, {}, ITarefa>, res: Response) => {
     
+    let validatedData: ITarefa | undefined;
+    try {
+        validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        const errors: Record<string, string> = {};
 
+        yupError.inner.forEach(error => {
+            if (!error.path) return;
+
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+    }
+
+    console.log(validatedData);
+    
     return res.send('Create!');
 }
