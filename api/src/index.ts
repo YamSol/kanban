9ef059d0adc8ca73fcba69eac7baf3cb1d03fe12
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2024-04-29 04:04:17 -0300

    chore: pequenas mudanças e alterações

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,5 +2,5 @@ import { server } from "./server/Server";
 
 
 server.listen(process.env.PORT || 3333, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+    console.log(`App rodando em: http://localhost:${process.env.PORT || 3333}`);
 });
--- api/src/server/controllers/tarefas/getAll.ts
+++ api/src/server/controllers/tarefas/getAll.ts
@@ -1,8 +1,7 @@
 import { RequestHandler } from "express";
-import { StatusCodes } from "http-status-codes";
 
 const j = require("../../../../data/board-data.json");
 
 export const getAll: RequestHandler = (req, res) => {
-    res.status(StatusCodes.ACCEPTED).json(j);
-}
+    res.json(j);
+};
