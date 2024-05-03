d2330b3520ceeb4f4979ab93fe74976e958bbf1b
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2024-05-03 16:38:18 -0300

    feat: criando conexão com o banco de dados

---
+++ api/src/server/database/knex/Enviroment.ts
@@ -0,0 +1,31 @@
+import { Knex } from 'knex';
+import path from 'path';
+
+export const development: Knex.Config = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(__dirname, '..', '..', '..', 'database.sqlite'),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  pool: {
+    afterCreate: (conn: any, done: Function) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+      done();
+    },
+  },
+};
+
+export const test: Knex.Config = {
+  ...development,
+  connection: ':memory:',
+};
+
+// export const production: Knex.Config = {
+//   ...development,
+// };
