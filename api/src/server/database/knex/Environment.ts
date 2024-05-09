32bc56da7d542ec7c92a95ae2ffdd9097e613b15
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2024-05-09 09:22:00 -0300

    refactor(knex/Environment, knex/index.ts): removendo environments desnecessários

--- api/src/server/database/knex/Environment.ts
+++ api/src/server/database/knex/Environment.ts
@@ -1,38 +1,20 @@
 import { Knex } from 'knex';
 import path from 'path';
 
-export const development: Knex.Config = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
+export const production: Knex.Config = {
+  client: 'mysql',
   connection: {
-    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite'),
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: 'root',
+    password: process.env.MYSQL__ROOT_PASSWORD,
   },
+  useNullAsDefault: true,
   migrations: {
     directory: path.resolve(__dirname, '..', 'migrations'),
   },
   seeds: {
     directory: path.resolve(__dirname, '..', 'seeds'),
   },
-  pool: {
-    afterCreate: (conn: any, done: Function) => {
-      conn.run('PRAGMA foreign_keys = ON', done);
-      done();
-    },
-  },
-};
 
-export const test: Knex.Config = {
-  ...development,
-  connection: ':memory:',
-};
-
-export const production: Knex.Config = {
-  ...development,
-  client: 'mysql',
-  connection: {
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-  }
 };
--- api/src/server/database/knex/index.ts
+++ api/src/server/database/knex/index.ts
@@ -1,15 +1,4 @@
 import knex from 'knex';
-import { development, production, test } from './Environment';
+import { production } from './Environment';
 
-const getEnvironment = () => {
-  switch (process.env.NODE_ENV) {
-    case 'PRODUCTION':
-      return production;
-    case 'TEST':
-      return test;
-    default: // 'DEVELOPMENT'
-      return development;
-  }
-};
-
-export const Knex = knex(getEnvironment());
+export const Knex = knex(production);
